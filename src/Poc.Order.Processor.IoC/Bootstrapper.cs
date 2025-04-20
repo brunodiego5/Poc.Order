3726cd6e4b510117d6901da41cabc4d061ea6fd9
brunodiego5 <bruno_diego5@hotmail.com>
2025-04-19 21:48:35 -0300

    feat(fila): adicionei o subscriber no processor

---
+++ src/Poc.Order.Processor.IoC/Bootstrapper.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Poc.Order.Processor.Domain.Interfaces;
+using Poc.Order.Processor.Subscriber.Subscribers;
+using RabbitMQ.Client;
+
+namespace Poc.Order.Api.IoC
+{
+    public static class Bootstrapper
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            RegisterMappers(services);
+            RegisterMediatRs(services);
+            RegisterBus(services, configuration);
+            RegisterSubscribers(services);
+        }
+
+        private static void RegisterMappers(IServiceCollection services)
+        {
+            //services.AddAutoMapper(
+            //    typeof(CreatePedidoCommandToPedidoProfile).Assembly,
+            //    typeof(PedidoToPedidoModelProfile).Assembly,
+            //    typeof(PedidoToPedidoMessageProfile).Assembly
+            //);
+        }
+
+        private static void RegisterMediatRs(IServiceCollection services)
+        {
+            //services.AddMediatR(cfg =>
+            //{
+            //    cfg.RegisterServicesFromAssembly(typeof(CreatePedidoCommandHandler).Assembly);
+            //});
+        }
+
+        private static void RegisterBus(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IConnection>(sp =>
+            {
+                var factory = new ConnectionFactory
+                {
+                    HostName = configuration["RabbitMq:Host"],
+                    Port = int.Parse(configuration["RabbitMq:Port"]),
+                    UserName = configuration["RabbitMq:User"],
+                    Password = configuration["RabbitMq:Password"],
+                    VirtualHost = configuration["RabbitMq:VirtualHost"]
+
+                };
+
+                return factory.CreateConnection();
+            });
+
+        }
+
+        private static void RegisterSubscribers(IServiceCollection services)
+        {
+            services.AddScoped<IPedidoSubscriber, PedidoSubscriber>();
+        }
+    }
+}
